92f788c806a6dacaff7fcf10b417f34f36efed60
Naho <t.naho.1737@gmail.com>
2021-11-02 01:28:07 +0900

    implement ResultsPage

--- lib/constants.dart
+++ lib/constants.dart
@@ -15,3 +15,28 @@ const kLabelTextStyle = TextStyle(
   fontSize: 18.0,
   color: Color(0xFF808E98),
 );
+
+const kLargeButtonTextStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultTextStyle = TextStyle(
+  color: Color(0xFF24DB76),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+);
